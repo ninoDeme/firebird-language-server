fd3cd272070255ab397fdf0a10e85e85fc8c3e68
ninodemeterko <ninodemeterko@hotmail.com>
2023-08-03 01:42:01 -0300

    initial-commit

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import { createConnection, TextDocuments, ProposedFeatures, TextDocumentSyncKind, Diagnostic, Range } from 'vscode-languageserver/node';
+
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { Parser } from './parser';
+
+import util from 'util'
+import * as inspector from 'node:inspector';
+
+// Creates the LSP connection
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a manager for open text documents
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+// The workspace folder this server is operating on
+let workspaceFolder: string | null;
+
+documents.onDidOpen((event) => {
+    connection.console.log(`[Server(${process.pid}) ${workspaceFolder}] Document opened: ${event.document.uri}`);
+    inspector.open(9229, '127.0.0.1');
+    if (inspector.url()) {
+        connection.console.log(`Inpector open on: ${inspector.url()}`);
+    }
+});
+
+documents.onDidChangeContent((event) => {
+    connection.console.log('Document changed 2');
+    connection.console.log(`${event.document.getText()}`);
+    const res = new Parser(connection);
+    try {
+        const _parsed = res.parse(event.document);
+        console.log(_parsed[0]);
+        try {
+            connection.console.log(util.inspect(_parsed));
+        } catch (e: any) {
+            connection.console.error(e?.message ?? e.toString());
+        }
+        connection.sendDiagnostics({
+            uri: event.document.uri,
+            diagnostics: res.problems.map((prob => {
+                const range = <Range>{start: event.document.positionAt(prob.start), end: event.document.positionAt(prob.end)};
+                return <Diagnostic>{
+                    severity: prob.severity,
+                    range,
+                    message: prob.message,
+                };
+            }))
+        });
+    } catch (e: any) {
+        connection.console.error(e?.message ?? e.toString());
+        throw e;
+    }
+});
+
+documents.listen(connection);
+
+connection.onInitialize((params) => {
+    params.workspaceFolders?.forEach(workspaceFolder => {
+        connection.console.log(`[Server(${process.pid}) ${workspaceFolder}] Started and initialize received`);
+    });
+    return {
+        capabilities: {
+            textDocumentSync: {
+                openClose: true,
+                change: TextDocumentSyncKind.Full
+            },
+            diagnosticProvider: {
+                documentSelector: [
+                    'sql',
+                    {
+                        'pattern': '*.sql'
+                    }
+                ],
+                interFileDependencies: false,
+                workspaceDiagnostics: false
+            }
+        }
+    };
+});
+connection.listen();
---
+++ teste.sql
@@ -0,0 +1,5 @@
+select
+     tdestase,
+     asddqwd,
+
+from a asdddddqwdsddasaasdd
